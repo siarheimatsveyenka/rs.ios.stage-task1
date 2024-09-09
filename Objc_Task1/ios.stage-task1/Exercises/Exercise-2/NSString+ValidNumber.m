8243c06b39b892a27790c888ab9d4ade2c866007
siarheimatsveyenka <siarhei.matsveyenka@innowise.com>
2024-09-09 16:36:28 +0300

    feat: Implement function for determining if the string represents a valid number Implemented function for determining if the string represents a valid number Exercise: #2

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
@@ -3,7 +3,23 @@
 @implementation NSString (ValidNumber)
 
 - (BOOL)isValidNumber {
-    return false;
+    
+    const char *array = [self UTF8String];
+    BOOL containsDigits = false;
+    
+    if (self.length == 0) {
+        return false;
+    } else {
+        for (int i = 0; i < strlen(array) ; i++) {
+            if (isdigit(array[i])) {
+                containsDigits = true;
+            } else {
+                containsDigits = false;
+                break;
+            }
+        }
+    }
+    return containsDigits;
 }
 
 @end
