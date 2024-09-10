d53374e4686d5498bbecf81b7ff4dfba51d55864
siarheimatsveyenka <siarhei.matsveyenka@innowise.com>
2024-09-10 19:19:12 +0300

    feat: Implement function for converting number to reverse character array Implemented function for converting number to reverse character array

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -3,7 +3,29 @@
 @implementation NumberConverter
 
 - (NSArray *)numberConverter:(NSNumber *)number {
-    return nil;
+    NSInteger const intNumber = [number intValue];
+    NSInteger const positiveNumber = labs(intNumber);
+    NSNumber const *positiveNSNumber = @(positiveNumber);
+    NSString const *positiveNumberNSString = [positiveNSNumber stringValue];
+    NSMutableArray<NSString *> *stringArrayOfPositive = [NSMutableArray array];
+    NSMutableArray *result = [NSMutableArray arrayWithCapacity:positiveNumberNSString.length];
+    NSInteger index = 0;
+
+    for (NSUInteger i = 0; i < positiveNumberNSString.length; i++) {
+        NSString *character = [positiveNumberNSString substringWithRange:NSMakeRange(i, 1)];
+        [stringArrayOfPositive addObject:character];
+        [result addObject:@""];
+        NSLog(@"stringArrayOfPositive == %@", stringArrayOfPositive);
+        index = result.count;
+        NSLog(@"index =  %ld", (long)stringArrayOfPositive.count);
+
+    }
+    
+    for (NSString *item in stringArrayOfPositive) {
+        result[index - 1] = item;
+        index--;
+    }
+    return result;
 }
 
 @end
