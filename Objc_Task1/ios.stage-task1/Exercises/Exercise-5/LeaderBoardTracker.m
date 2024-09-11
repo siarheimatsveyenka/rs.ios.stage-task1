c476e4982ffcdfd3668929c4c67c14eaf8765f52
siarheimatsveyenka <siarhei.matsveyenka@innowise.com>
2024-09-11 10:34:49 +0300

    feat: Implement func that tracking player ranking for every game Implemented func that tracking player ranking for every game

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
@@ -10,7 +10,30 @@
 
 - (NSArray <NSNumber *> *)trackLeaderBoard:(NSArray <NSNumber *> *)rankedArray
                                playerArray:(NSArray <NSNumber *> *)playerArray {
-    return @[];
+    
+    NSMutableArray *resultRanks = [NSMutableArray array];
+    NSOrderedSet *ranksSet = [NSOrderedSet orderedSetWithArray:rankedArray];
+    NSMutableArray *ranksArray = [ranksSet mutableCopy];
+    
+    for (int i = 0; i < playerArray.count; i++) {
+        if(rankedArray.count == 0){
+            [resultRanks addObject:@1];
+        } else if ([ranksArray indexOfObject:playerArray[i]] < ranksArray.count) {
+            [resultRanks addObject:@([ranksArray indexOfObject:playerArray[i]] + 1)];
+        } else {
+            if ([playerArray[i] intValue] < [ranksArray.lastObject intValue]){
+                [resultRanks addObject:@(ranksArray.count + 1)];
+            } else {
+                for (NSUInteger y = 0; y < ranksArray.count; y++) {
+                    if ([playerArray[i] intValue] > [ranksArray[y] intValue]) {
+                        [resultRanks addObject:@(y + 1)];
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return resultRanks;
 }
 
 @end
